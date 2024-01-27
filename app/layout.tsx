bedff1f704242aefc4d2c8b520831c00942052c6
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-27 12:47:06 +0530

    feat: Add smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overscroll-x-none scroll-smooth">
       <body
         className={`${playfair.variable} ${nunito.variable} ${algerian.variable}`}
       >
