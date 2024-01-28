6f602483b77c0d363bace5ce90b5a70d0292db09
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-28 11:57:34 +0530

    feat: Increase font size

--- app/components/Heading2.tsx
+++ app/components/Heading2.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export default function Heading2({ children }: { children: ReactNode }) {
   return (
-    <h2 className="font-serif text-2xl font-semibold md:text-4xl">
+    <h2 className="font-serif text-3xl font-semibold md:text-4xl">
       {children}
     </h2>
   )
