f50166480119c72fec19f8d17e5013c023e7f534
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-28 11:57:51 +0530

    feat: Link contacts & add copyright

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,43 +14,50 @@ export default async function Footer() {
   } = settings.data
 
   return (
-    <footer
-      id="contact"
-      className="mt-16 flex flex-wrap justify-between gap-8 text-xl sm:text-base"
-    >
-      <div>
-        <Image
-          src="/logo.jpg"
-          alt="Rizvi Law College Logo"
-          width={112}
-          height={112}
-          className="aspect-square w-28"
-        />
-        <p className="max-w-[35ch]">{description}</p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <h3 className="font-serif text-xl font-semibold">
-          Contact information
-        </h3>
-        <div className="grid gap-2">
-          {emails.map(({ email }, i) => (
-            <p key={i}>{email}</p>
-          ))}
-          {numbers.map(({ number }, i) => (
-            <p key={i}>{number}</p>
-          ))}
+    <footer id="contact" className="mb-8 mt-16 grid gap-8 text-xl sm:text-base">
+      <div className="flex flex-wrap justify-between gap-8">
+        <div>
+          <Image
+            src="/logo.jpg"
+            alt="Rizvi Law College Logo"
+            width={112}
+            height={112}
+            className="aspect-square w-28"
+          />
+          <p className="max-w-[35ch]">{description}</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3 className="font-serif text-xl font-semibold">
+            Contact information
+          </h3>
+          <div className="grid gap-2">
+            {emails.map(({ email }, i) => (
+              <a key={i} href={`mailto:${email}`} className="hover:underline">
+                {email}
+              </a>
+            ))}
+            {numbers.map(({ number }, i) => (
+              <a key={i} href={`tel:${number}`} className="hover:underline">
+                {number}
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3 className="font-serif text-xl font-semibold">Navigation links</h3>
+          <div className="grid gap-2">
+            {navigation_items.map(({ link, label }, i) => (
+              <PrismicNextLink key={i} field={link} className="inline-block">
+                {label}
+              </PrismicNextLink>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <h3 className="font-serif text-xl font-semibold">Navigation links</h3>
-        <div className="grid gap-2">
-          {navigation_items.map(({ link, label }, i) => (
-            <PrismicNextLink key={i} field={link} className="inline-block">
-              {label}
-            </PrismicNextLink>
-          ))}
-        </div>
-      </div>
+      <p className="text-center">
+        Copyright &copy; {new Date().getFullYear()} Rizvi Law College. All
+        Rights Reserved
+      </p>
     </footer>
   )
 }
