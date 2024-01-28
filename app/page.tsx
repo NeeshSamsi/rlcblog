77fb3ab361276c5731ff08d95ecadd5b5242a071
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-28 12:04:07 +0530

    feat: Dynamic posts from Prismic

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,71 @@
 import { Metadata } from "next"
-import { SliceZone } from "@prismicio/react"
 
 import { createClient } from "@/prismicio"
-import { components } from "@/slices"
+
+import Post from "@/app/components/Post"
+import Heading2 from "@/app/components/Heading2"
 
 export default async function Page() {
   const client = createClient()
   const page = await client.getSingle("home")
+  const posts = await client.getAllByType("blog_posts", {
+    fetchLinks: ["authors.name", "authors.link"],
+  })
+  // @ts-ignore
+  const featuredPost = await client.getByID(page.data.featured_post.id, {
+    fetchLinks: ["authors.name", "authors.link"],
+  })
+
+  const {
+    last_publication_date,
+    uid,
+    // @ts-ignore
+    data: { cover_image, title, author, summary },
+  } = featuredPost
 
   return (
-    <div className="min-h-screen">
-      <SliceZone slices={page.data.slices} components={components} />
-    </div>
+    <>
+      <section className="my-12 md:my-16">
+        <Post
+          featured
+          last_publication_date={last_publication_date}
+          uid={uid}
+          cover_image={cover_image}
+          title={title}
+          author={author}
+          summary={summary}
+        />
+      </section>
+
+      <section className="space-y-6">
+        <Heading2>Latest Posts</Heading2>
+
+        <div className="grid gap-8 md:grid-cols-2">
+          {posts.map(
+            (
+              {
+                last_publication_date,
+                uid,
+                data: { cover_image, title, author, summary },
+              },
+              i,
+            ) => {
+              return (
+                <Post
+                  key={i}
+                  last_publication_date={last_publication_date}
+                  uid={uid}
+                  cover_image={cover_image}
+                  title={title}
+                  author={author}
+                  summary={summary}
+                />
+              )
+            },
+          )}
+        </div>
+      </section>
+    </>
   )
 }
 
